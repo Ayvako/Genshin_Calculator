c04c1493134ef74a4850d14e61cfb44b2698ee81
Ayvako <ayvako2022@gmail.com>
2026-03-19 23:01:53 +0200

    redesign AddMaterials dialog with input validation

--- Genshin Calculator/Presentation/Features/Dialogs/AddMaterialsDialogView.xaml.cs
+++ Genshin Calculator/Presentation/Features/Dialogs/AddMaterialsDialogView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Genshin_Calculator.Presentation.Features.Dialogs;
 
@@ -7,5 +9,29 @@ public partial class AddMaterialsDialogView : Window
     public AddMaterialsDialogView()
     {
         this.InitializeComponent();
+    }
+
+    private void NumericOnly_PreviewTextInput(object sender, TextCompositionEventArgs e)
+    {
+        Regex regex = new Regex("[^0-9]+");
+        e.Handled = regex.IsMatch(e.Text);
+    }
+
+    private void NumericOnly_Pasting(object sender, DataObjectPastingEventArgs e)
+    {
+        if (e.DataObject.GetDataPresent(typeof(string)))
+        {
+            string text = (string)e.DataObject.GetData(typeof(string));
+            Regex regex = new Regex("[^0-9]+");
+
+            if (regex.IsMatch(text))
+            {
+                e.CancelCommand();
+            }
+        }
+        else
+        {
+            e.CancelCommand();
+        }
     }
 }
